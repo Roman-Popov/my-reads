2d1cb140ccf25dfd4c8b26f9e54054f679691588
Roman Popov <popov.r.k@yandex.ru>
2018-07-24 22:42:58 +0300

    feat: Update search

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -5,12 +5,33 @@ import { Link } from 'react-router-dom'
 class SearchPage extends Component {
 
     state = {
-        query: ''
+        query: this.getCurrentURL(),
+        timer: '',
+        lastURL: ''
+    }
+
+    getCurrentURL() {
+        return document.URL.substr(document.URL.indexOf('search') + 7).replace('%20', ' ')
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // Need in case of navigation back and forth by browser buttons
+        if (this.getCurrentURL() !== this.state.lastURL) {
+            this.setState({ lastURL: this.getCurrentURL() })
+            return
+        }
+        if (this.state.lastURL !== prevState.lastURL) {
+            this.props.searchBooks(this.state.lastURL)
+            this.setState({ query: this.state.lastURL, lastURL: this.getCurrentURL() })
+        }
     }
 
     updateQuery = (query) => {
-        this.setState({ query: query })
-        this.props.searchBooks(query)
+        clearTimeout(this.state.timer);
+        const timer = setTimeout(() => {
+            this.props.changeUrl(this.state.query);
+        }, 1000);
+        this.setState({ query: query, timer: timer, })
     }
 
     render() {
