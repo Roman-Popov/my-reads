8b6676ca30353803546c632ffea361f5aa7f2e94
Roman Popov <popov.r.k@yandex.ru>
2018-07-25 23:33:27 +0300

    style: Changed select input style

--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react'
 
 class ShelfChanger extends Component {
 
-    getCurrShelfName(shelves, shelfId) {
-        const shelfObj = shelves.filter(obj => obj.shelf === shelfId)[0]
-        return shelfObj ? shelfObj.name : 'None'
-    }
-
     render() {
         const { book, currentShelf, changeShelf } = this.props;
 
@@ -16,12 +11,10 @@ class ShelfChanger extends Component {
             { shelf: 'read', name: 'Read' },
             { shelf: 'none', name: 'None' }
         ]
-        const currShelfName = this.getCurrShelfName(shelves, currentShelf)
 
         return (
             <div className="book-shelf-changer">
                 <select onChange={(e) => changeShelf(book, e.target.value)} defaultValue={currentShelf || 'none'} >
-                    <option value="move" disabled>Now on: {currShelfName}</option>
                     <option value="move" disabled>Move to...</option>
                     {
                         shelves.map(shelf => (
